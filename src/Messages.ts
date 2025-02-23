ad35cb2526309d278dac8832e456c7f7030ad828
Matt Taylor <matt@matthewjosephtaylor.com>
2025-02-23 13:30:12 -0600

    export parseSubject under Messages

--- src/Messages.ts
+++ src/Messages.ts
@@ -1,7 +1,9 @@
 import { connectEventListenerToSubjectRoot } from "./connectEventListenerToSubjectRoot";
 import { createConnection } from "./createConnection";
+import { parseSubject } from "./type/ParsedSubject";
 
 export const Messages = {
   createConnection,
   connectEventListenerToSubjectRoot,
+  parseSubject,
 };
