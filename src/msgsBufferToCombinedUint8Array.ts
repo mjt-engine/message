8d377affe8b5753c834052c22e42c2f3a300d332
Matt Taylor <matt@matthewjosephtaylor.com>
2025-07-11 17:29:37 -0500

    debug msg

--- src/msgsBufferToCombinedUint8Array.ts
+++ src/msgsBufferToCombinedUint8Array.ts
@@ -1,10 +1,13 @@
 import { isUndefined } from "@mjt-engine/object";
 
 export const msgsBufferToCombinedUint8Array = (buffer: Uint8Array[]) => {
+  console.log("msgsBufferToCombinedUint8Array: Buffer length:", buffer.length);
+  console.log("msgsBufferToCombinedUint8Array: Buffer contents:", buffer);
   if (buffer.some((m) => isUndefined(m))) {
     throw new Error("Incomplete messages in buffer");
   }
   const totalLength = buffer.reduce((acc, m) => acc + m.byteLength, 0);
+  console.log("msgsBufferToCombinedUint8Array: Total length:", totalLength);
   const combined = new Uint8Array(totalLength);
   let offset = 0;
   for (const item of buffer) {
